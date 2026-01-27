8ccbece5ee901a68cc3a4910f8520b9af1a121eb
Andrii Naida <work.andrew.naida@gmail.com>
2026-01-27 18:49:23 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-2xl font-bold text-blue-500">Hello World!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react(),
+    dts({
+      include: ["src"],
+      tsconfigPath: "./tsconfig.app.json",
+      rollupTypes: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: path.resolve(__dirname, "src/main.ts"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: [
+        "react",
+        "react/jsx-runtime",
+        "tailwindcss",
+        "tailwind-merge",
+        "react-dom",
+        "@tailwindcss/vite",
+      ],
+    },
+  },
+});
